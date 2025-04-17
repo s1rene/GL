2dc670ce60a9bc24478aef053a3d47f359377511
s1rene <71638008+s1rene@users.noreply.github.com>
2025-04-17 10:50:27 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,22 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        500: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
 });
